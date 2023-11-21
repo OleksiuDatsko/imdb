ee2450f74389e18c495b7525560608e99e9b851f
OleksiuDatsko <datskooleksiu@gmail.com>
2023-11-21 17:16:32 +0200

    updated app.py

--- app/app.py
+++ app/app.py
@@ -11,13 +11,12 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     config_data = {
-        "DEBUG": os.getenv("DEBUG"),
-        "SQLALCHEMY_TRACK_MODIFICATIONS": os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS"),
+        "DEBUG": os.getenv("DEBUG") == "True",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS") == "True",
         "SQLALCHEMY_DATABASE_URI": os.getenv("SQLALCHEMY_DATABASE_URI"),
     }
-
     create_app(config_data).run(
         port=int(os.getenv("APP_PORT", "8000")),
         debug=True,
-        host="0.0.0.0",
+        host=os.getenv("APP_HOST", "localhost"),
     )
